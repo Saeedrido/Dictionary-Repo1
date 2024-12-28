a9a6261a33470a6da398c472304f729b5fa69c09
Saeedrido <Saeedrido99@gmail.com>
2024-12-28 08:10:36 +0100

    Alphabet Chart

---
+++ Alphabets.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Collections
+{
+    
+public static class MyProgram
+{
+
+   public static void Main()
+    {
+    
+      Dictionary<char, string> letterToFruit = new Dictionary<char, string>();
+
+        
+        string[] fruits = new string[]
+        {
+            "Abbass", "Banana", "Cherry", "Date", "Elderberry", "Fig", "Grape", "Honeydew", "Indian Fig", "Jorda",
+            "Kiwi", "Lemon", "Mango", "Nectarine", "Olive", "Papaya", "Quince", "Raspberry", "Strawberry", "Tangerine",
+            "Uva", "Violet", "Watermelon", "Xigua", "Yam Bean", "Zucchini"
+        };
+
+        
+        for (int i = 0; i < 26; i++)
+        {
+            letterToFruit.Add((char)('A' + i), fruits[i]);
+        }
+
+        int counter = 0;
+        foreach (var item in letterToFruit)
+        {
+            
+                Console.WriteLine($"{counter + 1} - {item.Key} For {item.Value}");
+              counter++;
+            
+        };
+    }
+}};
